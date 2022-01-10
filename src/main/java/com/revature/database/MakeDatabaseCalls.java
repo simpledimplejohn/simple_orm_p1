6f12212f3adf16e6dda977d5a7b5ce5c4285e6c7
Simple Dimple John <simpledimplejohn@gmail.com>
2022-01-09 17:14:53 -0800

    sql string for create table works

--- src/main/java/com/revature/database/MakeDatabaseCalls.java
+++ src/main/java/com/revature/database/MakeDatabaseCalls.java
@@ -2,11 +2,11 @@ package com.revature.database;
 
 import java.lang.annotation.Annotation;
 
-
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 import org.apache.log4j.Logger;
 
@@ -21,83 +21,117 @@ public class MakeDatabaseCalls {
 	private static Logger logger = Logger.getLogger(MakeDatabaseCalls.class);
 
 	static ClassInspector CI = new ClassInspector();
-	
-	public String ifIdPresent (Class<?> clazz) {
-		String idString = "";
-		
+
+	public String[] getFieldTypes(Class<?> clazz) {
+
 		Field[] fields = clazz.getDeclaredFields();
-		
-		for(Field field : fields) {
+		String[] fieldArray = new String[fields.length];
+		for (int i = 0; i < fields.length; i++) {
+			if (fields[i].getType().equals(Boolean.TYPE)) {
+				fieldArray[i] = "bool";
+			}
+			if (fields[i].getType().equals(Integer.TYPE)) {
+				fieldArray[i] = "int";
+			}
+
+			if (fields[i].getType().equals(String.class)) {
+				fieldArray[i] = "VARCHAR";
+			}
+		}
+		return fieldArray;
+	}
+
+	public static String sqlString(Class<?> clazz) {
+		// get table name
+		String tableName = CI.getClassTableName(clazz);
+		// table name to sql string
+		String sqlString = "(CREATE TABLE " + tableName + " (";
+		// get all fields
+		Field[] fields = clazz.getDeclaredFields();
+		String[] fieldArray = new String[fields.length];
+		String[] nameArray = new String[fields.length];
+		// populate fieldArray and nameArray
+		for (int i = 0; i < fields.length; i++) {
+			nameArray[i] = returnColumnAnnotations(fields[i].getAnnotations());
+			if (fields[i].getType().equals(Boolean.TYPE)) {
+				fieldArray[i] = "bool";
+			}
+			if (fields[i].getType().equals(Integer.TYPE)) {
+				fieldArray[i] = "int";
+			}
+
+			if (fields[i].getType().equals(String.class)) {
+				fieldArray[i] = "VARCHAR";
+			}
+		}
+		// add each name and type to string
+		for (int i = 0; i < fields.length; i++) {
+			if (i == fields.length - 1) {
+				sqlString += " " + nameArray[i] + " " + fieldArray[i];				
+			} else {
+				sqlString += " " + nameArray[i] + " " + fieldArray[i] + ",";
+			}
+		}
+
+		sqlString += ")";
+		System.out.println(Arrays.toString(fieldArray));
+		System.out.println(Arrays.toString(nameArray));
+		return sqlString;
+	}
+
+	public String ifIdPresent(Class<?> clazz) {
+		String idString = "";
+
+		Field[] fields = clazz.getDeclaredFields();
+
+		for (Field field : fields) {
 			idString += returnIdFromFields(field.getAnnotations());
 		}
 
-		
 		return idString;
 	}
-	
+
 	public String returnIdFromFields(Annotation[] annotations) {
 		String Idstring = "";
-		
-		for(Annotation annotation : annotations) {
-			if(annotation.annotationType().equals(Id.class)) {
+
+		for (Annotation annotation : annotations) {
+			if (annotation.annotationType().equals(Id.class)) {
 				Idstring += "SERIAL primary key";
 			}
 		}
-		
+
 		return Idstring;
-		
+
 	}
-	
-/////// TEST OF SQL STATMENT /////////////////////////////
-	
-	public static String testSqlString(Class<?> clazz) {
-		
-		String tableName = CI.getClassTableName(clazz);
-		
-		String sqlString = "(CREATE TABLE " + tableName;
-		
-		Field[] fields = clazz.getDeclaredFields();
-		
-		for(Field field : fields) {
-			sqlString += returnColumnAnnotations(field.getAnnotations());
-		}
-		sqlString += " )";
-		
-		return sqlString;
-	}
-	
-	
+
 	/**
-	 * Analyzes the set of 
-	 * */
+	 * Analyzes the set of
+	 */
 	public static String returnColumnAnnotations(Annotation[] annotations) {
-		
+
 		StringBuilder builder = new StringBuilder();
-		for(Annotation annotation : annotations) {
+		for (Annotation annotation : annotations) {
 			if (annotation.annotationType().equals(Column.class)) {
-				builder.append(((Column)annotation).columnName());
+
+				builder.append(((Column) annotation).columnName());
 			}
 		}
-		
-		
+
 		return builder.toString();
 	}
-	
-	
+
 /////////////////////////////////////////////////////////////////////	
 
 	public static void MakeTableReflection(Class<?> clazz) {
 
 		String tableName = CI.getClassTableName(clazz);
 		System.out.println("table name here " + tableName);
-		
+
 		Field[] fields = CI.listFields(clazz);
-		
-		
-		
+
 		try {
 			Connection conn = ConnectionUtil.getConnection();
-			
+
 			String sql = "CREATE TABLE " + tableName + " ();";
 
 			PreparedStatement stmt = conn.prepareStatement(sql);
@@ -107,7 +141,7 @@ public class MakeDatabaseCalls {
 		} catch (SQLException e) {
 			logger.error("Unable to make table - SQL exception found");
 			e.printStackTrace();
-		}	
+		}
 
 	}
 
